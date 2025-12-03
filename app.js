6d496d1b4fd72c922b9297d6ccb2f848c27a4378
nikita <rainikita211@gmail.com>
2025-12-03 09:35:16 +0530

    Removed .env data and added variable

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors({
-  origin: "*",       
-  methods: "GET,POST",
+  origin: "https://portfolio-a42ab.web.app",       
+  methods: ["GET,POST"],
   allowedHeaders: "Content-Type,Authorization"
 }));
 
